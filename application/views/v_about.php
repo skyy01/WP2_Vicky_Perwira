ea23370e653d0ef3f538d0b304c1c9ed2c754e63
skyy01 <vickygrs01@gmail.com>
2021-09-21 01:56:11 +0700

    pertemuan4

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Vicky</li>
+        <li>Nama Belakang : Suzana</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Remaja Jatinegara Kaum Pulogadung </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Futsal</li>
+    </ul>
+</section>
